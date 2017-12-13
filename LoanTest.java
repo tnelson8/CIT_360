d78d071d94a550e09cf0a6ab0c64ccc9560a4dcd
Nelson-Tyler <tdn3399@gmail.com>
2017-12-12 21:58:46 -0700

    Add files via upload

--- LoanTest.java
+++ LoanTest.java
@@ -1,9 +1,11 @@
 package junit;
+import org.junit.*;
 import static org.junit.Assert.*;
-import  org.junit.*;
+import static org.junit.matchers.JUnitMatchers.*;
 
 public class LoanTest {
 	private double delta = .005;
+
 	
 	@BeforeClass
 	public static void setUp1() {
@@ -15,18 +17,69 @@ public class LoanTest {
 		System.out.println("Starting a test");
 	}
 	
+	Loan loan1 = new Loan(2.5, 30, 100000.00);
+	Loan loan2 = new Loan(2.5, 30, 200000.00);
+	Loan loan3 = new Loan(2.5, 30, 300000.00);
+	Loan loan4 = new Loan(2.5, 30, 400000.00);	
+	Loan loan5 = new Loan(2.0, 30, 600000.00);	
+	Loan loan6 = new Loan(2.5, 30, 500000.00);		
 	@Test
-	public void testLoan1() {
-		Loan loan = new Loan(2.5, 30, 100000.00);
-		assertEquals(395.12, loan.getMonthlyPayment(), delta);
-		assertEquals(142243.52, loan.getTotalPayment(), delta);
+	public void testEquals() {
+		
+		assertEquals(395.12, loan1.getMonthlyPayment(), delta);
+		assertEquals(142243.52, loan1.getTotalPayment(), delta);
 	}
 	
 	@Test
-	public void testLoan2() {
-		Loan loan = new Loan(2.5, 30, 200000.00);
-		assertEquals(790.24, loan.getMonthlyPayment(), delta);
-		assertEquals(284487.05, loan.getTotalPayment(), delta);
+	public void testArrayEquals() {
+		long paymentArray1[];
+		long loanArray2[];
+		//Obviously these will match because they are the same loans being compared. 
+		paymentArray1 = new long[] {395,790,1185};
+		loanArray2 = new long[] {(long) loan1.getMonthlyPayment(),(long) loan2.getMonthlyPayment(),(long) loan3.getMonthlyPayment()};
+		assertArrayEquals(paymentArray1, loanArray2);
+	}
+	
+	@Test
+	public void testFalse() {
+		System.out.println("Monthly Payment should be less than your total");
+		
+		assertFalse(loan1.getTotalPayment() < loan1.getLoanAmount());
+	}
+	
+	@Test
+	public void testNotNull() {
+		System.out.println("Make sure a loan has been created");
+		assertNotNull(loan1);
+	}
+	
+	@Test
+	public void testNotSame() {
+		assertNotSame(loan1, loan2);
+	}
+	
+	@Test
+	public void testNull() {
+		assertNull(null);
+	}
+	@Test
+	public void testSame() {
+		assertSame(loan1,loan1);
+	}
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testThat() {
+		String info = "4805557736";
+		String areaCode = "480";
+		//Make sure the number given is an Arizona 480 number
+	    assertThat(info, containsString(areaCode));
+	}
+	
+	@Test
+	public void testTrue() {
+		double loanBalance = 86347.54;
+		//Assure that there is still money on the loan
+		assertTrue(0 < loanBalance);
 	}
 	
 	@AfterClass
